635126a44ff76d2aae0eb584a1c9c2ee57eaef9f
TomaszKorwel <tomasz@korwel.net>
2014-02-20 11:11:04 -0600

    Update Package.php
    The change creates session variable that stores the state of the code and prevents the code from executing more than once per session. It also prevents known issue with Varnish integration.

--- app/code/local/Creare/Abtesting/Model/Package.php
+++ app/code/local/Creare/Abtesting/Model/Package.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 class Creare_Abtesting_Model_Package extends Mage_Core_Model_Design_Package
 {
-	public function getTheme($type)
+    public function getTheme($type)
     {
         if (empty($this->_theme[$type])) {
-            $this->_theme[$type] = Mage::getStoreConfig('design/theme/'.$type, $this->getStore());
-            if ($type!=='default' && empty($this->_theme[$type])) {
+            $this->_theme[$type] = Mage::getStoreConfig('design/theme/' . $type, $this->getStore());
+            if ($type !== 'default' && empty($this->_theme[$type])) {
                 $this->_theme[$type] = $this->getTheme('default');
                 if (empty($this->_theme[$type])) {
                     $this->_theme[$type] = self::DEFAULT_THEME;
@@ -23,53 +23,57 @@ class Creare_Abtesting_Model_Package extends Mage_Core_Model_Design_Package
         if ($customThemeType) {
             $this->_theme[$type] = $customThemeType;
         }
-		
-		/* Our cookie code */
-		
-		$cookie = Mage::getModel('core/cookie')->get('abtesting');
-		$temp = Mage::getSingleton('core/session')->getAbTesting();
-		
-		if(Mage::getStoreConfig('abtesting/accounts/active')){
-				$replace = Mage::getSingleton('core/design_package')->getPackageName()."/";	
-				if(!$cookie){
-					$test = Mage::getStoreConfig('abtesting/current/test');
-					setcookie('abtesting',$test);
-					Mage::getSingleton('core/session')->setAbTesting('yes'); // temporarily set this to yes - so as not to double up this code
-					if($test == "a"){
-						$code = Mage::getStoreConfig('abtesting/accounts/acode');
-						Mage::getSingleton('core/session')->setAbTest('a');
-						if($code && !empty($code)){
-							$this->_theme[$type] = str_replace($replace,"",Mage::getStoreConfig('abtesting/accounts/adesign'));
-						}
-						Mage::getConfig()->saveConfig('abtesting/current/test','b');
-					} else {
-						$code = Mage::getStoreConfig('abtesting/accounts/bcode');
-						Mage::getSingleton('core/session')->setAbTest('b');
-						if($code && !empty($code)){
-							$this->_theme[$type] = str_replace($replace,"",Mage::getStoreConfig('abtesting/accounts/bdesign'));
-						}
-						Mage::getConfig()->saveConfig('abtesting/current/test','a');
-					}
-				} else {
-					$test = $cookie;
-					if($test == "a"){
-						$code = Mage::getStoreConfig('abtesting/accounts/acode');
-						if($code && !empty($code)){
-							$this->_theme[$type] = str_replace($replace,"",Mage::getStoreConfig('abtesting/accounts/adesign'));
-						}
-					} else {
-						$code = Mage::getStoreConfig('abtesting/accounts/bcode');
-						if($code && !empty($code)){
-							$this->_theme[$type] = str_replace($replace,"",Mage::getStoreConfig('abtesting/accounts/bdesign'));
-						}
-					}
-				}
-				
-		}
-		
-		/* end our cookie code */
-		
+
+        /* Our cookie code */
+
+        $cookie = Mage::getModel('core/cookie')->get('abtesting');
+        $temp = Mage::getSingleton('core/session')->getAbTesting();
+
+        if (Mage::getStoreConfig('abtesting/accounts/active')) {
+            if (!Mage::getSingleton('core/session')->getAbTestingTheme()) {
+                $replace = Mage::getSingleton('core/design_package')->getPackageName() . "/";
+                if (!$cookie) {
+                    $test = Mage::getStoreConfig('abtesting/current/test');
+                    Mage::getModel('core/cookie')->set('abtesting', $test);
+                    Mage::getSingleton('core/session')->setAbTesting('yes'); // temporarily set this to yes - so as not to double up this code
+                    if ($test == "a") {
+                        $code = Mage::getStoreConfig('abtesting/accounts/acode');
+                        Mage::getSingleton('core/session')->setAbTest('a');
+                        if ($code && !empty($code)) {
+                            $this->_theme[$type] = str_replace($replace, "", Mage::getStoreConfig('abtesting/accounts/adesign'));
+                        }
+                        Mage::getConfig()->saveConfig('abtesting/current/test', 'b');
+                    } else {
+                        $code = Mage::getStoreConfig('abtesting/accounts/bcode');
+                        Mage::getSingleton('core/session')->setAbTest('b');
+                        if ($code && !empty($code)) {
+                            $this->_theme[$type] = str_replace($replace, "", Mage::getStoreConfig('abtesting/accounts/bdesign'));
+                        }
+                        Mage::getConfig()->saveConfig('abtesting/current/test', 'a');
+                    }
+                } else {
+                    $test = $cookie;
+                    if ($test == "a") {
+                        $code = Mage::getStoreConfig('abtesting/accounts/acode');
+                        if ($code && !empty($code)) {
+                            $this->_theme[$type] = str_replace($replace, "", Mage::getStoreConfig('abtesting/accounts/adesign'));
+                        }
+                    } else {
+                        $code = Mage::getStoreConfig('abtesting/accounts/bcode');
+                        if ($code && !empty($code)) {
+                            $this->_theme[$type] = str_replace($replace, "", Mage::getStoreConfig('abtesting/accounts/bdesign'));
+                        }
+                    }
+                }
+                Mage::getSingleton('core/session')->setAbTestingTheme($this->_theme[$type]);
+            } else {
+                $this->_theme[$type] = Mage::getSingleton('core/session')->getAbTestingTheme();
+            }
+        }
+
+        /* end our cookie code */
+
         return $this->_theme[$type];
     }
-	
+
 }
